fb2f6c77e6bd8aea41b55ce6cc0d0a251292abc1
Govardhan Chitrada <codebreaker@CB-MAC.local>
2019-09-29 21:24:00 +0530

    New post added

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,20 @@
-set -e # halt script on error
+  
+#!/bin/bash
 
-bundle exec jekyll build
-bundle exec htmlproofer ./_site
+if [[ $TRAVIS_BRANCH == 'master' ]] ; then
+  cd _site
+  git init
+
+  git config user.name "codebreaker444"
+  git config user.email "zeroerror.444@gmail.com"
+
+  git add .
+  git commit -m "Deploy"
+
+  # We redirect any output to
+  # /dev/null to hide any sensitive credential data that might otherwise be exposed.
+  git push --force --quiet "https://${git_user}:${git_password}@${git_target}" master:master > /dev/null 2>&1
+else
+  echo 'Invalid branch. You can only deploy from gh-pages.'
+  exit 1
+fi
