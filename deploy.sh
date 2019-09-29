7b7c6db706cbd0c3e7cf1a3c1c13dc336835c6c0
Govardhan Chitrada <codebreaker@CB-MAC.local>
2019-09-29 22:25:31 +0530

    New post added

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-if [ $TRAVIS_BRANCH == 'master' ]  then
   cd _site
   git init
 
@@ -12,7 +11,3 @@ if [ $TRAVIS_BRANCH == 'master' ]  then
   # We redirect any output to
   # /dev/null to hide any sensitive credential data that might otherwise be exposed.
   git push --force --quiet "https://${git_user}:${git_password}@${git_target}" master:master > /dev/null 2>&1
-else
-  echo 'Invalid branch. You can only deploy from gh-pages.'
-  exit 1
-fi
