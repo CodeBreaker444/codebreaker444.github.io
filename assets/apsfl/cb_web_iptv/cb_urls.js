d10ef3aeb6ff6d9a85cbc32845cf12fe31ab0c79
CodeBreaker <CodeBreaker444@users.noreply.github.com>
2020-04-02 13:30:36 +0530

    Add files via upload

---
+++ assets/apsfl/cb_web_iptv/cb_urls.js
@@ -0,0 +1,81 @@
+url="https://cbproxycors.herokuapp.com/http://103.211.109.68:8888/content02/SERVICE_ASSET_FILES/LTV/ltv_allchannellist_OTHER_DEVICE.json";
+//url_test="http://localhost/web_apfiber/chan.json"
+var len;
+function append_json(data){
+  var table = document.getElementById('gable');
+  var tbody = document.getElementById('gable1');
+  len=Object.keys(data.Channel).length;
+
+  var i;
+  for(i=0;i<Object.keys(data.Channel).length-1;i++) {
+    //var tbody = document.createElement('tbody');
+    //table.appendChild(tbody);
+
+      var tr = document.createElement('tr');
+      //tbody.appendChild('tr');
+      tr.innerHTML =  '<td style="text-align:center;">' + data.Channel[i].channelNo+ '</td>' +
+      '<td>' + data.Channel[i].name + '</td>' +
+      '<td style="text-align:center;" onclick="CopyMyLeftTd(this)">' + data.Channel[i].streamProfile[0].urltype[0].value + '</td>' +
+      '<td style="text-align:center;">' + data.Channel[i].language + '</td>';
+      tbody.appendChild(tr);
+  }
+}
+
+fetch(url)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data['Channel'][0]['name']);
+        var loader_main=document.getElementById('loader');
+        loader_main.style.display="none"
+        append_json(data);
+    })
+
+    function myFunction() {
+      // Declare variables
+      var input, filter, ul, li, a, i, txtValue;
+      input = document.getElementById('myInput');
+      filter = input.value.toUpperCase();
+      ul = document.getElementById("gable1");
+      li = ul.getElementsByTagName('tr');
+    
+      // Loop through all list items, and hide those who don't match the search query
+      for (i = 0; i < len; i++) {
+        a = li[i].getElementsByTagName("td")[1];
+        a_lang = li[i].getElementsByTagName("td")[3];
+
+        console.log(a);
+        txtValue = a.textContent || a.innerText;
+        txtValue_lang=a_lang.textContent || a_lang.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1 || txtValue_lang.toUpperCase().indexOf(filter) > -1 ) {
+          li[i].style.display = "";
+
+        } else {
+          li[i].style.display = "none";
+
+
+        }
+
+      }
+    }
+    
+      function CopyMyLeftTd(txt) {
+       var el = document.createElement('textarea');
+       var q = document.getElementById('copied');
+       var q_copy = document.getElementById('copy');
+       
+       q_copy.style.display="none";
+
+       
+        el.value = txt.innerHTML;
+        q.innerHTML = el.value+" Copied!";
+
+        el.setAttribute('readonly', '');
+        el.style.position = 'absolute';
+        el.style.left = '-9999px';
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+      }
---
+++ assets/apsfl/cb_web_iptv/cb_urls_jquery.js
@@ -0,0 +1,19 @@
+$("#search").on("keyup", function() {
+    var value = $(this).val();
+
+    $("table tr").each(function(index) {
+        if (index !== 0) {
+
+            $row = $(this);
+
+            var id = $row.find("td:first").text();
+
+            if (id.indexOf(value) !== 0) {
+                $row.hide();
+            }
+            else {
+                $row.show();
+            }
+        }
+    });
+});
